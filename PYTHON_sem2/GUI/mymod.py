fef9a1b766ab8d96a9c963ff3089d52f28d18d0a
ISHUAGRAWAL1122 <142567471+ISHUAGRAWAL1122@users.noreply.github.com>
2024-04-08 23:43:57 +0530

    Delete PYTHON_sem2 directory

--- PYTHON_sem2/GUI/mymod.py
+++
@@ -1,6 +0,0 @@
-def prime(x):
-    a=[i for i in range(1,x+1) if x%i==0]
-    if len(a)>2:
-        return 'Not Prime'
-    else:
-        return 'Prime'
--- PYTHON_sem2/mymod/test.py
+++
@@ -1,4 +0,0 @@
-def add(a,b):
-    return f'Addition of {a} and {b} is {a+b}'
-if __name__=='__main__':
-    print("hello world")
